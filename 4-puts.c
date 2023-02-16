2b32125f049ab65e2e6cba26819939e133d9d3de
Farahd Amos <amoshasim2012@gmail.com>
2023-02-16 02:56:55 -0800

    low level C programming

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** 
+ * main - main function
+ * puts - puts function, prints string
+ * Return: 0 on success
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
